0f9461c55be6b5f065eba8d7dbf75de5b3287f0b
Lauric <62937013+Lauric-h@users.noreply.github.com>
2024-02-29 09:05:40 +0100

    add server listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 use crate::models::food::IngestionType;
 use crate::dto::food_list_dto::FoodListDTO;
 use crate::models::food::Food;
@@ -6,19 +8,36 @@ mod models;
 mod repositories;
 
 fn main() {
-    println!("Hello, world!");
-    let c: FoodListDTO = FoodListDTO {
-        foods: vec![],
-    };
+    let listener = TcpListener::bind(format!("0.0.0.0:8081")).unwrap();
+    println!("Server listening on port 8080");
 
-    let f: Food = Food {
-        id: 0,
-        name: "".to_string(),
-        ingestion: IngestionType::EAT,
-        carbs: 0,
-        calories: 0,
-        proteins: 0,
-        electrolytes: false,
-        comment: "".to_string(),
-    };
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("OK stream");
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let mut request = String::new();
+
+    match stream.read(&mut buffer) {
+        Ok(size) => {
+            request.push_str(String::from_utf8_lossy(&buffer[..size]).as_ref());
+
+            let content = match &*request {
+                r if r.starts_with("GET /health") => "get".to_string(),
+                _ => "Not found".to_string()
+            };
+            stream.write_all(format!("{}", content).as_bytes()).unwrap();
+        }
+        Err(e) => eprintln!("Unable to read stream: {}", e),
+    }
 }
